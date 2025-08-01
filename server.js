189cbaa0da8d53f7e2036c48a503bf4b565a20fc
nikof7 <40549719+nikof7@users.noreply.github.com>
2025-08-01 14:30:25 -0300

    added cors

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
+const cors = require("cors");
+
 const express = require("express");
 const routes = require("./routes");
+
 const APP_PORT = process.env.APP_PORT || 3000;
+
 const app = express();
 
-// app.use(cors()); // Para habilitar esta línea es necesario instalar la librería `cors`.
+app.use(cors()); // Para habilitar esta línea es necesario instalar la librería `cors`.
 app.use(express.static("public"));
 app.use(express.json());
 
