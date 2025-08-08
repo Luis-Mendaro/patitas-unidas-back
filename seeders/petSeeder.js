fa4c5ea00262f0f1c50231d9e98ec21348606037
nikof7 <40549719+nikof7@users.noreply.github.com>
2025-08-08 14:49:36 -0300

    update pet seeder

--- seeders/petSeeder.js
+++ seeders/petSeeder.js
@@ -20,6 +20,12 @@ module.exports = async () => {
 
     const imagePool = petImages[species] || petImages.other;
 
+    const options = [
+      { value: 1, weight: 0.6 },
+      { value: 2, weight: 0.3 },
+      { value: 3, weight: 0.1 },
+    ];
+
     pets.push({
       name: faker.animal.petName(),
       description: faker.lorem.paragraph(),
@@ -31,7 +37,11 @@ module.exports = async () => {
       size: faker.helpers.arrayElement(sizes),
       color: faker.color.human(),
       age: faker.number.int({ min: 3, max: 180 }),
+<<<<<<< Updated upstream
       isAdopted: faker.datatype.boolean(0.2),
+=======
+      isAdopted: faker.datatype.boolean(),
+>>>>>>> Stashed changes
       categoryId: faker.helpers.weightedArrayElement(options),
       shelterUserId: faker.helpers.arrayElement(shelters).id,
     });
