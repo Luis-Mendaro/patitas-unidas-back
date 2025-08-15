189596a582f7b0c86d17245a11c0c477e55c448e
nikof7 <40549719+nikof7@users.noreply.github.com>
2025-08-15 12:14:42 -0300

    Move store route to unprotected

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,12 @@ const userController = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
 const authorizeRoles = require("../middlewares/authorizeRolesMiddleware");
 
-router.use(authMiddleware);
+router.post("/", userController.store);
 
+router.use(authMiddleware);
 router.use(authorizeRoles(["admin", "user"]));
 
 router.get("/", userController.index);
-router.post("/", userController.store);
 router.get("/:id", userController.show);
 router.patch("/:id", userController.update);
 router.delete("/:id", userController.destroy);
