93215585fb8d53aa3539d9109171e3d6a47946f6
Luis Mendaro <secureddev.lm@gmail.com>
2026-07-20 23:32:33 -0300

    feat: add temporary /seed endpoint to populate demo data

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,20 @@ module.exports = (app) => {
   app.use("/pets", petRoutes);
   app.use("/auth", authRoutes);
   app.use("/requests", requestRoutes);
+
+  app.post("/seed", async (req, res) => {
+    try {
+      await require("../seeders/userSeeder")();
+      await require("../seeders/shelterUserSeeder")();
+      await require("../seeders/categorySeeder")();
+      await require("../seeders/alternativePetSeeder")();
+      await require("../seeders/likedPetsSeeder")();
+      await require("../seeders/productSeeder")();
+      await require("../seeders/requestSeeder")();
+      res.json({ msg: "Seeders corridos correctamente." });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: "Error al correr seeders.", error: error.message });
+    }
+  });
 };
