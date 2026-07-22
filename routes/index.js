f3f36f928a1b91f7bb09355ef830f6d0fcc71dcb
Luis Mendaro <secureddev.lm@gmail.com>
2026-07-22 09:20:34 -0300

    fix: seed endpoint continues on individual seeder errors

--- routes/index.js
+++ routes/index.js
@@ -12,18 +12,19 @@ module.exports = (app) => {
   app.use("/requests", requestRoutes);
 
   app.post("/seed", async (req, res) => {
-    try {
-      await require("../seeders/userSeeder")();
-      await require("../seeders/shelterUserSeeder")();
-      await require("../seeders/categorySeeder")();
-      await require("../seeders/alternativePetSeeder")();
-      await require("../seeders/likedPetsSeeder")();
-      await require("../seeders/productSeeder")();
-      await require("../seeders/requestSeeder")();
-      res.json({ msg: "Seeders corridos correctamente." });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ msg: "Error al correr seeders.", error: error.message });
+    const results = {};
+    const seeders = [
+      "userSeeder", "shelterUserSeeder", "categorySeeder",
+      "alternativePetSeeder", "likedPetsSeeder", "productSeeder", "requestSeeder"
+    ];
+    for (const name of seeders) {
+      try {
+        await require(`../seeders/${name}`)();
+        results[name] = "ok";
+      } catch (e) {
+        results[name] = e.message;
+      }
     }
+    res.json(results);
   });
 };
